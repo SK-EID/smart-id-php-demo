032a170b5fb88e4308f6c8113b932bd01dd1f9c3
andrevka <andreas.valdma@nortal.com>
2019-09-04 15:37:31 +0300

    add authentication validation, change setup.sh from dos to unix line endings, make setup.sh check for bower and composer packages separately

--- backend/app/auth/validate-action.php
+++ backend/app/auth/validate-action.php
@@ -26,18 +26,24 @@ $app->post( 'validate', function() use ( $app, $client )
   {
       $authenticationResponseValidator = new AuthenticationResponseValidator();
       $authenticationResult = $authenticationResponseValidator->validate( $authenticationResponse );
+      $isValidResult = $authenticationResult->isValid();
       $authenticationIdentity = $authenticationResult->getAuthenticationIdentity();
       $app['session']->set( 'user', $authenticationIdentity );
 
       $response = array(
-          'data' => array(
+          'data' => ($isValidResult) ? array(
               'isSignedIn'   => true,
               'firstName'    => $authenticationIdentity->getGivenName(),
               'lastName'     => $authenticationIdentity->getSurName(),
               'personalCode' => $authenticationIdentity->getIdentityCode()
-          )
-      );
+          ) :
+          array(
+              'isSignedIn'  => false,
+              'errors' => $authenticationResult->getErrors(),
+              )
+          );
   }
 
-  return $app->json( array_merge( $response, array( 'status' => 'SUCCESS' ) ) );
+
+  return $app->json(array_merge($response, array( 'status' => (($isValidResult) ? 'SUCCESS' : 'ERROR')) ), $isValidResult ? 200 : 401);
 } );
--- docker/setup.sh
+++ docker/setup.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env sh
 
-if [ ! -d '/app/bower_components' ]; then
-  NODE_VERSION="7.6.0"
-
+if [ ! -d '/app/backend/vendor' ]; then
+  cd /app
   cd backend
   curl -sS https://getcomposer.org/installer | php
   php composer.phar install
+  cd /../..
+fi
+
+if [ ! -d '/app/bower_components' ]; then
+  NODE_VERSION="7.6.0"
 
   # Setup demo app frontend
   cd /app
